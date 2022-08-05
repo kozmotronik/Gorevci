1650a44ad2143ae18b80b9fc5ee1637a276c8af9
Kozmotronik <ismailsahillioglu@gmail.com>
2022-08-05 23:15:15 +0300

    Hata düzelt ve iyileştirmeler yap.
    + grvOlustur işlevinde aynı is_t türünde bir görev zaten varsa NULL döndürmek yerine kullanışlılığı artırmak için GKB referanını döndür.
    
    + Doxygen belgelendirmesini tamamla.
    
    + Kaynak dosyasındaki girintileme (indent) sorunlarnı düzelt.

--- kaynak/gorev.c
+++ kaynak/gorev.c
@@ -1,26 +1,33 @@
 /**
  * Görevci v1.0.0
- * Copyright (C) 2021 İsmail Sahillioğlu (aka Kozmotronik). Tüm Hakları Saklıdır.
- * Hiçbir ücret talep edilmeden burada işbu* yazılımın bir kopyasını ve belgelendirme
- * dosyalarını (“Yazılım”) elde eden herkese verilen izin; kullanma, kopyalama,
- * değiştirme, birleştirme, yayımlama, dağıtma, alt lisanslama, ve/veya yazılımın
- * kopyalarını satma eylemleri de dahil olmak üzere ve bununla kısıtlama olmaksızın,
- * yazılımın sınırlama olmadan ticaretini yapmak için verilmiş olup, bunları yapmaları
- * için yazılımın sağlandığı kişilere aşağıdakileri yapmak koşuluyla sunulur:
- * 
- * Yukarıdaki telif hakkı bildirimi ve işbu izin bildirimi yazılımın tüm kopyalarına
- * veya önemli parçalarına eklenmelidir.
- * 
- * YAZILIM “HİÇBİR DEĞİŞİKLİK YAPILMADAN” ESASINA BAĞLI OLARAK, TİCARETE ELVERİŞLİLİK,
- * ÖZEL BİR AMACA UYGUNLUK VE İHLAL OLMAMASI DA DAHİL VE BUNUNLA KISITLI OLMAKSIZIN
- * AÇIKÇA VEYA ÜSTÜ KAPALI OLARAK HİÇBİR TEMİNAT OLMAKSIZIN SUNULMUŞTUR. HİÇBİR KOŞULDA
- * YAZARLAR VEYA TELİF HAKKI SAHİPLERİ HERHANGİ BİR İDDİAYA, HASARA VEYA DİĞER
- * YÜKÜMLÜLÜKLERE KARŞI, YAZILIMLA VEYA KULLANIMLA VEYA YAZILIMIN BAŞKA BAĞLANTILARIYLA
- * İLGİLİ, BUNLARDAN KAYNAKLANAN VE BUNLARIN SONUCU BİR SÖZLEŞME DAVASI, HAKSIZ FİİL
- * VEYA DİĞER EYLEMLERDEN SORUMLU DEĞİLDİR.
- ***************************************************************************************
- * 
+ * Copyright (C) 2021 İsmail Sahillioğlu (aka Kozmotronik). Tüm Hakları
+ * Saklıdır. Hiçbir ücret talep edilmeden burada işbu* yazılımın bir kopyasını
+ * ve belgelendirme dosyalarını (“Yazılım”) elde eden herkese verilen izin;
+ * kullanma, kopyalama, değiştirme, birleştirme, yayımlama, dağıtma, alt
+ * lisanslama, ve/veya yazılımın kopyalarını satma eylemleri de dahil olmak
+ * üzere ve bununla kısıtlama olmaksızın, yazılımın sınırlama olmadan
+ * ticaretini yapmak için verilmiş olup, bunları yapmaları için yazılımın
+ * sağlandığı kişilere aşağıdakileri yapmak koşuluyla sunulur:
+ *
+ * Yukarıdaki telif hakkı bildirimi ve işbu izin bildirimi yazılımın tüm
+ * kopyalarına veya önemli parçalarına eklenmelidir.
+ *
+ * YAZILIM “HİÇBİR DEĞİŞİKLİK YAPILMADAN” ESASINA BAĞLI OLARAK, TİCARETE
+ * ELVERİŞLİLİK, ÖZEL BİR AMACA UYGUNLUK VE İHLAL OLMAMASI DA DAHİL VE BUNUNLA
+ * KISITLI OLMAKSIZIN AÇIKÇA VEYA ÜSTÜ KAPALI OLARAK HİÇBİR TEMİNAT OLMAKSIZIN
+ * SUNULMUŞTUR. HİÇBİR KOŞULDA YAZARLAR VEYA TELİF HAKKI SAHİPLERİ HERHANGİ BİR
+ * İDDİAYA, HASARA VEYA DİĞER YÜKÜMLÜLÜKLERE KARŞI, YAZILIMLA VEYA KULLANIMLA
+ * VEYA YAZILIMIN BAŞKA BAĞLANTILARIYLA İLGİLİ, BUNLARDAN KAYNAKLANAN VE
+ * BUNLARIN SONUCU BİR SÖZLEŞME DAVASI, HAKSIZ FİİL VEYA DİĞER EYLEMLERDEN
+ * SORUMLU DEĞİLDİR.
  */
+
+
+/**
+ * \ingroup gorev
+ * @{
+ */
+
 
 #include "gorev.h"
 #include "portmacro.h"
@@ -43,83 +50,84 @@ static unsigned char gorevSayimi = 0;
 
 
 pgkb_t grvOlustur(is_t is) {
-	pgkb_t gkb;
-	// Görev listesi dolu, NULL döndür.
-	if(gorevSayimi >= grvMAX_GOREV_SAYISI) return NULL;
-	/* Aynı iş parçasına sahip bir görev oluşturulmuş mu? */
-	for(char s = 0; s < gorevSayimi; s++) {
-		if(is == gorevHavuzu[s].is) {
-			/* Aynı iş parçasına sahip bir görev zaten var */
-			return NULL;
-		}
-	}
-	/* Her şey tamam, yeni görevi oluştur ve ilkle */
-	gkb = &gorevHavuzu[gorevSayimi];
-	gkb->kimlik = gorevSayimi;
-	gkb->durum = grvBEKLIYOR;
-	gkb->is = is;
-	grvILKLE(gkb);
-	gorevSayimi++;
-	return gkb;
+    pgkb_t gkb;
+    // Görev listesi dolu, NULL döndür.
+    if(gorevSayimi >= grvMAX_GOREV_SAYISI) return NULL;
+    /* Aynı iş parçasına sahip bir görev oluşturulmuş mu? */
+    for(char s = 0; s < gorevSayimi; s++) {
+        if(is == gorevHavuzu[s].is) {
+            /* Aynı iş parçasına sahip bir görev zaten var */
+            return &gorevHavuzu[s];
+        }
+    }
+    /* Her şey tamam, yeni görevi oluştur ve ilkle */
+    gkb = &gorevHavuzu[gorevSayimi];
+    gkb->kimlik = gorevSayimi;
+    gkb->durum = grvBEKLIYOR;
+    gkb->is = is;
+    grvILKLE(gkb);
+    gorevSayimi++;
+    return gkb;
 }
 
 void grvBaslat(const unsigned char kimlik) {
-	pgkb_t gkb = grvKimlikIleGorevBlogunuAl(kimlik);
-	if(gkb == NULL) return;
-	grvILKLE(gkb);
-	gkb->durum = grvBEKLIYOR;
+    pgkb_t gkb = grvKimlikIleGorevBlogunuAl(kimlik);
+    if(gkb == NULL) return;
+    grvILKLE(gkb);
+    gkb->durum = grvBEKLIYOR;
 }
 
 void grvDurdur(const unsigned char kimlik) {
-	pgkb_t gkb = grvKimlikIleGorevBlogunuAl(kimlik);
-	if(gkb == NULL) return;
-	gkb->durum = grvBITTI;
+    pgkb_t gkb = grvKimlikIleGorevBlogunuAl(kimlik);
+    if(gkb == NULL) return;
+    gkb->durum = grvBITTI;
 }
 
 void grvGorevciyiBaslat() {
-	while(1) {
-		for(unsigned char s = 0; s < gorevSayimi; s++) {
-			/* Sıradaki Görev Kontrol Bloğunu al */
-			pgkb_t gkb = &gorevHavuzu[s];
-			/* Bir şekilde NULL olmuş görev kontrol bloğu, sonrakine geç */
-			if(gkb == NULL) continue;
-			/* Görevin iş parçasını al */
-			is_t is = gkb->is;
-			/* Bir şekilde NULL olmuş iş parçası, sonrakine geç */
-			if(is == NULL) continue;
-			/* Görev etkin mi? Değilse sonrakini başlat */
-			char durum = gkb->durum;
-			if(durum == grvBITTI || durum == grvCIKTI) continue;
-			durum = is(gkb); // Sıradaki görevi çalıştır.
-			/* Görevin durumunu sakla */
-			gkb->durum = durum;
-		}
-	}
+    while(1) {
+        for(unsigned char s = 0; s < gorevSayimi; s++) {
+            /* Sıradaki Görev Kontrol Bloğunu al */
+            pgkb_t gkb = &gorevHavuzu[s];
+            /* Bir şekilde NULL olmuş görev kontrol bloğu, sonrakine geç */
+            if(gkb == NULL) continue;
+            /* Görevin iş parçasını al */
+            is_t is = gkb->is;
+            /* Bir şekilde NULL olmuş iş parçası, sonrakine geç */
+            if(is == NULL) continue;
+            /* Görev etkin mi? Değilse sonrakini başlat */
+            char durum = gkb->durum;
+            if(durum == grvBITTI || durum == grvCIKTI) continue;
+            durum = is(gkb); // Sıradaki görevi çalıştır.
+            /* Görevin durumunu sakla */
+            gkb->durum = durum;
+        }
+    }
 }
 
 pgkb_t grvKimlikIleGorevBlogunuAl(const unsigned char kimlik) {
-	if(kimlik >= grvMAX_GOREV_SAYISI) return NULL;
-	/* Blok havuzda var öyleyse GKB'na başvuru döndür */
-	return &gorevHavuzu[kimlik];
+    if(kimlik >= grvMAX_GOREV_SAYISI) return NULL;
+    /* Blok havuzda var öyleyse GKB'na başvuru döndür */
+    return &gorevHavuzu[kimlik];
 }
 
 
 #endif
 
 unsigned int grvTikSayiminiAl() {
-	unsigned int sayim;
-	// Atomik bölge giriş
-	portKRITIK_BOLUM_GIRISI();
-	sayim = tikSayimi;
-	// Atomik bölge çıkış
-	portKRITIK_BOLUM_CIKISI();
-	return sayim;
+    unsigned int sayim;
+    // Atomik bölge giriş
+    portKRITIK_BOLUM_GIRISI();
+    sayim = tikSayimi;
+    // Atomik bölge çıkış
+    portKRITIK_BOLUM_CIKISI();
+    return sayim;
 }
 
 /**
  * Bu işlev sistem sürevcisi kesmesinden çağrılır.
  */
 void grvTikKesmeIsleyici() {
-	tikSayimi++;
+    tikSayimi++;
 }
 
+/// @}
